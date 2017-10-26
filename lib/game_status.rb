33370cb33079b4d3e56d282c81b1dcd02c9a772d
Hoon Lee <yl.hoony@gmail.com>
2017-10-26 05:58:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,5 +20,11 @@ def won?(board)
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    
+    
   end
 end
