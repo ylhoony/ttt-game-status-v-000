a1aa0ccda3fc906a0f08f688c84cf8e232b1b741
Hoon Lee <yl.hoony@gmail.com>
2017-10-26 07:25:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,21 +15,29 @@ WIN_COMBINATIONS = [
   [2,4,6],
 ]
 
+# def won?(board)
+#   WIN_COMBINATIONS.detect do |win_combination|
+#     win_index_1 = win_combination[0]
+#     win_index_2 = win_combination[1]
+#     win_index_3 = win_combination[2]
+# 
+#     position_1 = board[win_index_1]
+#     position_2 = board[win_index_2]
+#     position_3 = board[win_index_3]
+# 
+#     if position_1 == "X" && position_2 =="X" && position_3 == "X" || position_1 == "O" && position_2 =="O" && position_3 == "O"
+#       win_combination
+#     else
+#       false
+#     end
+#   end
+# end
+
 def won?(board)
-  WIN_COMBINATIONS.detect do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
-
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 =="X" && position_3 == "X" || position_1 == "O" && position_2 =="O" && position_3 == "O"
-      win_combination
-    else
-      false
-    end
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
   end
 end
 
@@ -52,7 +60,7 @@ end
 #   game_status = won?(board)
 #   el = game_status[0]
 #   # board[el]
-# 
+#
 #   if !!game_status
 #     board[el][0]
 #   end
